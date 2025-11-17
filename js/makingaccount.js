6d7095535c3e5177044d67a78b7c5c01893d6533
Zeghilet Afrah <afrah.zeghilet@ensia.edu.dz>
2025-11-17 14:21:41 +0100

    initial commit 4

--- js/makingaccount.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("login-architect").addEventListener("submit", function(e) {
-  e.preventDefault(); 
-
-  const formData = new FormData(this);
-  fetch("saveClient.php", { method: "POST", body: formData });
-  
-  window.location.href = "architect-interface.html";
-});
